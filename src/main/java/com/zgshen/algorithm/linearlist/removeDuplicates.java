aa3a87dae24f34f4580d36997b0404fafe86ebe3
shen <zguishen@foxmail.com>
2019-11-17 00:00:48 +0800

    no message

--- src/main/java/com/zgshen/algorithm/linearlist/removeDuplicates.java
+++ src/main/java/com/zgshen/algorithm/linearlist/removeDuplicates.java
@@ -13,7 +13,26 @@ public class removeDuplicates {
         if (nums.length == 0) return 0;
         int index = 1;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[index -1]) {
+            if (nums[i] != nums[index - 1]) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+
+    /**
+     * Follow up for "Remove Duplicates": What if duplicates are allowed at most twice?
+     * For example, given sorted array A = [1,1,1,2,2,3] , your function should return length = 5 , and A is
+     * now [1,1,2,2,3]
+     * 允许两个重复的
+     * @param nums
+     * @return
+     */
+    public static int solution1(int nums[]) {
+        if (nums.length <= 2) return nums.length;
+        int index = 2;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[index - 2]) {
                 nums[index++] = nums[i];
             }
         }
@@ -22,6 +41,8 @@ public class removeDuplicates {
 
     public static void main(String[] args) {
         int nums[] = {1, 5, 5, 5, 5, 7, 8, 23, 33};
+        int nums1[] = {1, 5, 5, 5, 5, 7, 8, 23, 33};
         System.out.println(solution(nums));
+        System.out.println(solution1(nums1));
     }
 }
