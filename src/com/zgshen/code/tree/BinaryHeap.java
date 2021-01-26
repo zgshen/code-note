2dff63d536098bd3e183fe500207fd65cd376a67
zgshen <zguishen@foxmail.com>
2021-01-26 20:53:30 +0800

    队列

---
+++ src/com/zgshen/code/tree/BinaryHeap.java
@@ -0,0 +1,78 @@
+package com.zgshen.code.tree;
+
+import java.util.Arrays;
+
+/**
+ * 漫画算法：小灰算法之旅
+ * 二叉堆
+ * 数组存储
+ * 假设父节点下标是 parent，那么它的左孩子下标就是 2× parent+1；右孩子下标就是 2× parent+2。
+ */
+public class BinaryHeap {
+
+    /**
+     * 插入操作，上浮调整
+     */
+    public static void upAdjust(int[] array) {
+        int childIndex = array.length - 1;//插入节点下标，数组最后一个元素
+        int parentIndex = (childIndex - 1)/2;//父节点下标
+
+        int temp = array[childIndex];//调整的数
+        while (childIndex>0 && temp < array[parentIndex]) {
+            array[childIndex] = array[parentIndex];
+            childIndex = parentIndex;
+            parentIndex = (parentIndex - 1)/2;
+        }
+        array[childIndex] = temp;
+    }
+
+    /**
+     * 下沉调整
+     * 删除操作，最后一个元素替换一个元素再做下沉操作
+     */
+    public static void downAdjust(int[] array, int parentIndex, int length) {
+        int temp = array[parentIndex];
+        int childIndex = 2 * parentIndex + 1;//左节点
+        //判断是否属于数组
+        while (childIndex < length) {
+            //判断右节点是否小于左节点，成立定位到右节点
+            if (childIndex + 1 < length && array[childIndex + 1] < array[childIndex]) {
+                childIndex ++;
+            }
+            //父节点小于任意子节点，不必操作
+            if (temp <= array[childIndex]) {
+                break;
+            }
+            //父大于子，下沉
+            array[parentIndex] = array[childIndex];
+            //继续下一层遍历判断
+            parentIndex = childIndex;
+            childIndex = 2 * parentIndex + 1;
+        }
+        array[parentIndex] = temp;
+    }
+
+    /**
+     * 构建堆，下沉操作
+     * @param array
+     */
+    public static void buildHeap(int[] array) {
+        for (int i = (array.length-2)/2; i >= 0; i--) {
+            downAdjust(array, i, array.length);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(3/2);
+
+        int[] array = new int[] {1,3,2,6,5,7,8,9,10,0};
+        upAdjust(array);
+        System.out.println(Arrays.toString(array));
+
+        array = new int[] {7,1,3,10,5,2,8,9,6};
+        buildHeap(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+
+}
---
+++ src/com/zgshen/code/tree/PriorityQueue.java
@@ -0,0 +1,96 @@
+package com.zgshen.code.tree;
+
+import java.util.Arrays;
+
+/**
+ * 优先队列
+ */
+public class PriorityQueue {
+
+    private int[] array;
+    private int size;
+
+    public PriorityQueue() {
+        array = new int[32];
+    }
+
+    //入队
+    public void enQueue(int key) {
+        if (size > array.length) {
+            //扩容
+        }
+        array[size++] = key;
+        //上浮操作
+        upAdjust();
+    }
+
+    //出队
+    public int deQueue() throws Exception{
+        if (size <= 0) throw new Exception("empty queue...");
+        //堆顶元素
+        int head = array[0];
+        //最后一个元素替代
+        array[0] = array[--size];
+        //下沉操作
+        downAdjust();
+        return head;
+    }
+
+    /**
+     * 上浮
+     */
+    private void upAdjust() {
+        int childIndex = size - 1;
+        int parentIndex = (childIndex - 1) / 2;
+        int temp = array[childIndex];
+        while (childIndex > 0 && temp > array[parentIndex]) {
+            array[childIndex] = array[parentIndex];
+            childIndex = parentIndex;
+            parentIndex = parentIndex / 2;
+        }
+        array[childIndex] = temp;
+    }
+
+    /**
+     * 下沉
+     */
+    private void downAdjust() {
+        int parentIndex = 0;
+        int temp = array[parentIndex];
+        int childIndex = 1;
+        while (childIndex < size) {
+            if (childIndex + 1 < size && array[childIndex+1] > array[childIndex]) {
+                childIndex++;
+            }
+            if (temp > array[childIndex]) break;
+            array[parentIndex] = array[childIndex];
+            parentIndex = childIndex;
+            childIndex = 2 * parentIndex + 1;
+        }
+        array[parentIndex] = temp;
+    }
+
+    private void resize() {
+        int newSize = size * 2;
+        this.array = Arrays.copyOf(this.array, newSize);
+    }
+
+    public static void main(String[] args) throws Exception {
+        PriorityQueue priorityQueue = new PriorityQueue();
+        priorityQueue.enQueue(3);
+        priorityQueue.enQueue(10);
+        priorityQueue.enQueue(5);
+        priorityQueue.enQueue(2);
+        priorityQueue.enQueue(8);
+
+        System.out.println(Arrays.toString(priorityQueue.array));
+
+        System.out.println("出队" + priorityQueue.deQueue());
+        System.out.println("出队" + priorityQueue.deQueue());
+        System.out.println(Arrays.toString(priorityQueue.array));
+
+        priorityQueue.enQueue(1);
+        priorityQueue.enQueue(12);
+        System.out.println(Arrays.toString(priorityQueue.array));
+    }
+}
