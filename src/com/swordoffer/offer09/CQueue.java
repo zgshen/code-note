8c5338f53387df06126de3a965cd6e990e84a569
zgshen <zguishen@foxmail.com>
2021-04-27 10:01:54 +0800

    feat：两栈实现队列

---
+++ src/com/swordoffer/offer09/CQueue.java
@@ -0,0 +1,64 @@
+package com.swordoffer.offer09;
+
+import com.zgshen.typora.C;
+
+import java.util.Stack;
+
+/**
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ *
+ * 示例 1：
+ * 输入：
+ * ["CQueue","appendTail","deleteHead","deleteHead"]
+ * [[],[3],[],[]]
+ * 输出：[null,null,3,-1]
+ * 示例 2：
+ *
+ * 输入：
+ * ["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+ * [[],[],[5],[2],[],[]]
+ * 输出：[null,-1,null,null,5,2]
+ * 提示：
+ *
+ * 1 <= values <= 10000
+ * 最多会对 appendTail、deleteHead 进行 10000 次调用
+ *
+ */
+public class CQueue {
+
+    Stack<Integer> a, b;
+
+    public CQueue() {
+        a = new Stack<>();
+        b = new Stack<>();
+    }
+
+    public void appendTail(int value) {
+        a.push(value);
+    }
+
+    public int deleteHead() {
+        if (b.isEmpty()) {
+            while (!a.isEmpty()) {
+                b.push(a.pop());
+            }
+        }
+
+        if (b.isEmpty()) {
+            return -1;
+        }
+
+        return b.pop();
+    }
+
+
+    public static void main(String[] args) {
+        CQueue cQueue = new CQueue();
+        cQueue.appendTail(1);
+        cQueue.appendTail(2);
+        cQueue.appendTail(3);
+        System.out.println(cQueue.a);
+
+        System.out.println(cQueue.deleteHead());
+    }
+}
