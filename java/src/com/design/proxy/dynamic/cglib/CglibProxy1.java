83b272c93927dda91499502747b3247d7fd56b00
zgshen <zguishen@foxmail.com>
2023-09-20 17:35:45 +0800

    cglib proxy

---
+++ java/src/com/design/proxy/dynamic/cglib/CglibProxy1.java
@@ -0,0 +1,35 @@
+package com.design.proxy.dynamic.cglib;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class CglibProxy1 implements MethodInterceptor {
+
+    private Object target;
+
+    public CglibProxy1(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("Before do something, do something");
+        Object invoke = method.invoke(target, objects);
+        System.out.println("After do something, do something");
+        return invoke;
+    }
+
+    public Object getProxyInstance() {
+        //工具类
+        Enhancer en = new Enhancer();
+        //设置父类
+        en.setSuperclass(target.getClass());
+        //设置回调函数
+        en.setCallback(this);
+        //创建子类对象代理
+        return en.create();
+    }
+}
